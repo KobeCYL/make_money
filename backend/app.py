ea2552f85cebe6f51026034841f252ea1af5e3d6
kobe <417125111@qq.com>
2025-09-05 13:13:41 +0800

    feat: perfact demo module

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 from routers.call_routers import call_bp
+from routers.demo_routes import demo_dp
 app = Flask(__name__)
 
 
 app.register_blueprint(call_bp)
+app.register_blueprint(demo_dp)
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'message': f'Not Found: {str(error)}'}), 404
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({'message': f'Bad Request: {str(error)}'}), 400
+
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({'message': f'Internal Server Error: {str(error)}'}), 500
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
