d750348699c62af0fa3a3b3c94d1ea25cd2a7764
kobe <417125111@qq.com>
2025-09-05 19:51:19 +0800

    feat: 添加点名的逻辑

--- backend/routers/call_routers.py
+++ backend/routers/call_routers.py
@@ -1,21 +1,85 @@
 from flask import  Blueprint, request, jsonify, abort
-import json
+import json, time, random
 
 call_bp = Blueprint('call', __name__)
 
+class JsonOptions:
+    def __init__(self, path,):
+        self.path = path
+        self.jsonObj = None
+    def __enter__(self):
+        self.jsonObj =  open(self.path, 'r', encoding='utf-8') 
+        return self.jsonObj
+    def __exit__(self, exc_type, exc_val, exc_tb):
 
+        self.jsonObj.close()
+
+    def get_json(self):
+        return json.load(self.jsonObj)
+
+    def set_json(self, data):
+        json.dump(data, self.jsonObj, ensure_ascii=False)
+
+name_path = 'data/name.json'
+
+lib_path = 'data/call.json'
+    
 @call_bp.route('/call', methods=['GET'])
 def call():
     return jsonify({
         'status': '200'
     })
 
-# 获取随机面试官
-@call_bp.route('/roll-call/interviewers', methods=['POST'])
-def call():
-    data = request.get_data()
-    print('data', data)
 
+
+# 获取随机面试官
+@call_bp.route('/roll-call/interviewers', methods=['GET'])
+def call():
+    lib = JsonOptions(lib_path)
+    call_data = lib.get_json()
+    data = request.get_json()
+    call_history = call_data['call_history']
+    interview_history = call_data['interview_history']
+    interview_history.concat(call_history)
+    count = data['count']
+    # 随机获取count个面试官
+    can_call_list = []
+    total_list = JsonOptions(name_path).get_json()
+
+    currentTime = time.time()
+    for item in total_list:
+        if item['name'] not in can_call_list:
+            can_call_list.append(item)
+    # 获取随机名单
+    index_list = []
+    for i in range(count):
+        index = random.random(0,len(can_call_list)-1)
+        currentItem = can_call_list.pop(index)
+        index_list.append(currentItem)
+        
+    interview_history.concat(index_list)
+
+    # 保存表
+    lib.set_json({
+        'call_history': call_history,
+        'interview_history': interview_history
+    })
+
+    return jsonify({
+        'code': 200,
+        'data': index_list,
+        'message': 'ok'
+    })
+
+   
+
+    # 获取3天前的时间
+    threeDayTime = currentTime - 60 * 60 * 24 * 3
+
+    # 判断一个时间是在3天内
+  
+
+    data = request.get_data()
     try:
         data = json.loads(data)
 
